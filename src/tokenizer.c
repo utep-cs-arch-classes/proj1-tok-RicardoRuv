c98d0ccdb51e8aa699b6bed30ecfde97e66ff7f8
Ricardo Ruvalcaba <rruvalcaba2@utep.miners.edu>
2021-02-20 01:46:25 -0700

    Adding tokenizer.c [implements methods in tokenizer.h]

---
+++ src/tokenizer.c
@@ -0,0 +1,13 @@
+#include "tokenizer.h"
+
+int space_char(char c){
+  if(c == ' ' || c == '\t' || c == '\n')
+    return 1;
+  return 0;
+}
+
+int non_space_char(char c){
+  if( c != ' ' || c !='\t' || c !='\n')
+    return 1;
+  return 0;
+}
