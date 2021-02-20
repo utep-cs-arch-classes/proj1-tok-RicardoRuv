2a479af376831f70c10c2ce2ca5cb85267ecf336
Ricardo Ruvalcaba <rruvalcaba2@utep.miners.edu>
2021-02-20 01:31:31 -0700

    Created tokenizer.c which will implement the methods in Tokenizer.h   |  Created input.c to test methods implemented.

---
+++ src/input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "tokenizer.h"
+
+int main(){
+
+  int input;
+  input = getchar();
+
+  int r = space_char(input);
+
+  printf("%d\n",r);
+
+  
+}
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,14 +1,23 @@
-#include <stdio.h>
+#include "tokenizer.h"
 
-
-int main(){
-  printf(">");
-
- int input;
- input= getchar();
-    
-  while (input != EOF){
-    putchar(input);
-    input = getchar();
-  }
+int space_char(char c){
+  // true if c is a tab or space, and not zero
+  if(c == ' ' || c == '\t' || c == '\n')
+    return 1;
+  return 0;
 }
+
+/* Return true (non-zero) if c is a non-whitespace
+
+   character (not space, tab, or newline).
+
+   Zero terminators are not printable (therefore false) */
+
+int non_space_char(char c){
+  if(c != ' ' && c != '\t' && c != '\n'){
+    return 1;
+  return 0;
+
+}
+
+
