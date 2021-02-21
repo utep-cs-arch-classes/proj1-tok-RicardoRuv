f732829c41ba1f9f012f6f3153bf8ff17dd2b0e9
Ricardo Ruvalcaba <rruvalcaba2@utep.miners.edu>
2021-02-21 02:03:57 -0700

    Tested methods

--- src/input.c
+++ src/input.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include "tokenizer.h"
+#define LIMIT 100
+
 
 int main(){
 
-  int input;
+  /* int input;
   input = getchar();
 
   int r = space_char(input);
 
   printf("%d\n",r);
+  */
 
+  char arrayC[LIMIT];
+  int i=0;
+
+
+  for(char singleChar; (singleChar = getchar()) != '\n' && i < LIMIT - 1; i++){
+    arrayC [i] = singleChar;
+  }
+  char *pointer = word_start(arrayC);
+  printf("This is the first char: %c\n", *pointer);
+
+  char *endPointer = word_end(arrayC);
   
+  printf("This is the last char: %c\n", *endPointer);
+
+  int numOfWords = count_words(arrayC);
+  printf("Total words: %i\n", numOfWords);
 }
