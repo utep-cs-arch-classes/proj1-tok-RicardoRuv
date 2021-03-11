320a92ebef65f53c52d8a902f7dcfa265bc927b3
Ricardo Ruvalcaba <rruvalcaba2@utep.miners.edu>
2021-03-11 15:51:48 -0700

    Added remaining methods for history.c

--- src/history.c
+++ src/history.c
@@ -5,9 +5,8 @@
 
 List *init_history(){
   
- List  *list = malloc(sizeof(List)*1);
- list->root = (Item*)malloc(sizeof(Item));
-
+  List  *list =(List*)malloc(sizeof(List)*1);
+  list ->root = (Item*) malloc(sizeof(Item)*1);
 
   return list;
 }
@@ -19,17 +18,49 @@ List *init_history(){
 
 void add_history(List *list, char *str){
   
-  Item *temp = list->root;
-  List *ptr;
+  Item *temp = (Item*)malloc(sizeof(Item)*1);
+  temp ->str = str;
+  
+  Item *ptr = list->root;
+  int i =1;
+
+  if(ptr == NULL){
+    temp ->id = i;
+    ptr = temp;
+    return;
+  }
+    while(ptr != NULL && ptr->next != NULL){
+      i++;
+      ptr = ptr->next;
+    }
+    temp->id=i;
+    ptr->next = temp;
 }
 
-
 void print_history(List *list){
-
   Item *temp = list -> root;
-
-  while(temp -> next != NULL){
-    printf("%s\n id: token: ",temp->id, temp -> str);
+  while(temp != NULL){
+    printf("%d: %s \n ",temp->id,temp->str);
     temp = temp -> next;
   }
 }
+
+void free_history(List *list){
+  free(list);
+}
+
+/* Retrieve the string stored in the node where Item->id == id.
+
+   List* list - the linked list
+
+   int id - the id of the Item to find */
+
+char *get_history(List *list, int id){
+  Item *tmp = list->root;
+
+  while(tmp->id != id){
+    tmp=tmp->next;
+  }
+  return tmp->str;
+}
+
