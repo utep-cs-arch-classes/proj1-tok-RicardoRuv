b7de2d27431983b80648af6a2e158d3cc79e69a5
Ricardo Ruvalcaba <rruvalcaba2@utep.miners.edu>
2021-03-11 17:05:18 -0700

    Implemented history methods

--- src/input.c
+++ src/input.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "tokenizer.h"
+#include "history.h"
 #define LIMIT 100
 
 
@@ -20,6 +21,8 @@ int main(){
   for(char singleChar; (singleChar = getchar()) != '\n' && i < LIMIT - 1; i++){
     arrayC [i] = singleChar;
   }
+  printf("  Testing tokenizer.c methods:\n");
+  
   char *pointer = word_start(arrayC);
   printf("This is the first char: %c\n", *pointer);
 
@@ -29,4 +32,36 @@ int main(){
 
   int numOfWords = count_words(arrayC);
   printf("Total words: %i\n", numOfWords);
+
+  char *clone =copy_str(arrayC,5);
+  printf("copyStr: %s\n",clone);
+
+  char *str = "hey there tom my name is tom";
+
+  printf("  Testing tokenizer on string given \n");
+  char *ptr = arrayC;
+  char **tokens = tokenize(ptr);
+  print_tokens(tokens);
+
+  printf(" Testing history:\n");
+
+  char r[LIMIT]= "Hello tom my name is tom";
+  char u[LIMIT]= "This is my third string";
+  char  *s2=r;
+  char *s3 =u;
+  List *li = init_history();
+  add_history(li,ptr);
+  add_history(li,s2);
+  add_history(li,s3);
+  print_history(li);
+
+  printf("  Testing get history: id:2\n");
+  char *v = get_history(li,2);
+  printf("%s\n ",v);
+
+  
+  printf("  Testing free history:\n");
+  free_history(li);
+  print_history(li);
+
 }
